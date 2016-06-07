e7a440d86af4a47cdeb17d576325457612115ee7
filipesantiago <filipe_santiago@id.uff.br>
2016-06-07 02:34:12 -0300

    Faz o SplitList retornar os arquivos, agora é fazer isso virar uma lista e largar pro array to sequence fazer o trabalho dele

--- Trabalho 1 - Astronomia/BatFamily/SplitList.bat
+++ Trabalho 1 - Astronomia/BatFamily/SplitList.bat
@@ -1,2 +1,13 @@
+@echo off
+
 call PSLoadExecutable.bat SplitList -o FileEntry?.xml -f ReadCSVReadyFileOutput.xml
 IF NOT ERRORLEVEL 0 GOTO ENDERR
+
+set myvar=
+for /r %%i in (FileEntry*.xml) DO call :concat %%i
+echo %myvar%
+goto :eof
+
+:concat
+set myvar=%myvar% %1;
+goto :eof
